27ed1e9b7d597a758b452122b57bd314a5dd88b9
Crimars <crimars@yandex.ru>
2025-05-19 22:57:45 +0300

    creat

---
+++ src/main/java/web/config/CarService.java
@@ -0,0 +1,26 @@
+package web.config;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+    public class CarService {
+        private final List<Car> cars = new ArrayList<>(Arrays.asList(
+                new Car(1, "Kia", 2010),
+                new Car(2, "Honda", 2019),
+                new Car(3, "Ford", 2021),
+                new Car(4, "BMW", 2018),
+                new Car(5, "Lada", 2015)
+        ));
+    public List<Car> getNCars(int count) {
+        if (count >= cars.size()) {
+            return cars;
+        } else {
+            return cars.subList(0, count);
+        }
+    }
+}
+
---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,28 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.config.Car;
+import web.config.CarService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cars")
+public class CarsController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public String getCars(@RequestParam(name = "count", required = false) Integer count, Model model) {
+        int size = (count != null && count > 0) ? Math.min(count, 5) : 5;
+        List<Car> cars = carService.getNCars(size);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
