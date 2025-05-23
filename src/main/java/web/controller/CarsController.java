08e233b36905e938f79e40060cdcac7e3cf645c0
Crimars <crimars@yandex.ru>
2025-05-23 22:05:07 +0300

    creat 2

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,8 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.config.Car;
-import web.config.CarService;
+import web.model.Car;
+import web.service.CarService;
 
 import java.util.List;
 
